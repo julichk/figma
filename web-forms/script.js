eaa1e13bd085c88e983fce3c88e95fc1e8888fb9
“julichk” <julia01.smith01@gmail.com>
2023-04-11 15:16:28 +0300

    style form

---
+++ web-forms/script.js
@@ -0,0 +1,13 @@
+console.log('35' + - '22');
+console.log('35' * "22");
+
+
+
+let userCounter = 0;
+let newUser = userCounter++;
+console.log(newUser);
+
+console.log(!false && 11 || 18 && !'');
+
+let love = null;
+console.log(love ?? "Без імені");
