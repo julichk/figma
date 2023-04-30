6e1a5a59138c35653d3ba4d29fa557e6a3c82af4
Yulia <104633872+julichk@users.noreply.github.com>
2023-04-30 18:32:53 +0300

    add consts

--- firstJS/scripts/firstJS.js
+++ firstJS/scripts/firstJS.js
@@ -1,13 +1,13 @@
 
-radius = prompt('Привіт, дай радіус кола','число');
+const radius = prompt('Привіт, дай радіус кола','число');
 alert(`Площа кола дорівнює ${radius*Math.PI**2}`);
 
-distance = prompt('Cкільки км до міста?','число');
-time = prompt('За скільки годин хочеш дістатися туди?','число');
+const distance = prompt('Cкільки км до міста?','число');
+const time = prompt('За скільки годин хочеш дістатися туди?','число');
 alert(`Ти добрешся з такою швидкістю: ${distance/time}`);
 
 const exchangeRrate = 0.90;
 
-dollar = prompt('Скільки доларів цікавить?','число');
+const dollar = prompt('Скільки доларів цікавить?','число');
 alert(`Ось скільки це буде євро: ${dollar * exchangeRrate}`);
 
