008a405624de81e587359b6ed115ffddda9f20c1
“julichk” <julia01.smith01@gmail.com>
2023-05-15 11:00:09 +0300

    done

---
+++ arrays/scripts/arrays.js
@@ -0,0 +1,31 @@
+const allProducts = ["шампунь", "кондиціонер", "порошок", "паста"];
+console.log(allProducts);
+//завдання 1
+const deleteProduct = allProducts.splice(1, 1);
+console.log(allProducts);
+
+//ЗАВДАННЯ 2
+
+// const newProduct = prompt('новий продукт','')
+
+// if(newProduct !== ''){
+//   allProducts.push(newProduct);
+// }
+// const price = [12];
+
+// for(i=0; i<allProducts.length; i++){
+
+//   document.write(allProducts[i] + ": " + price + "<br>");
+// } ;
+
+// const total = price * allProducts.length;
+// document.write('Всього до сплати: ' + total);
+// allProducts.forEach(function(product) {
+//   document.write(product.name + ": " + product.price + "<br>");
+// });
+
+// products.forEach(function(entry) {
+//   console.log(entry);
+// });
+//console.log(newProduct);
+//console.log(addNewProduct);
