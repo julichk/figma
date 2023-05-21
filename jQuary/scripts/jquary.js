bf6fda8f8c9614bd9eef6b2c7e64dd61d40f25f3
“julichk” <julia01.smith01@gmail.com>
2023-05-22 01:49:11 +0300

    footer

--- jQuary/scripts/jquary.js
+++ jQuary/scripts/jquary.js
@@ -1,40 +1,31 @@
 
-// const swiper = new Swiper('.swiper', {
-//     direction: 'horizontal',
-//     loop: true,
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
 
 
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
-
-// const swiperBaner = new Swiper('.baner', {
-//   direction: 'horizontal',
-//   loop: true,
-//   effect: 'cube',
-//   autoplay: {
-//     delay: 2000,
-//   },
-//   pagination: {
-//     el: '.baner .swiper-pagination',
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: '.baner .swiper-pagination',
-//     prevEl: '.baner .swiper-pagination',
-//     },
-
-// });
-$(document).ready(function() {
-  
-  $('.center').slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-    nextArrow: '<button type="button" class="slick-next">Next</button>'
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
-  
+
+const swiperBaner = new Swiper('.baner', {
+  direction: 'horizontal',
+  loop: true,
+  effect: 'slide',
+  autoplay: {
+    delay: 5000, // Змініть значення delay на 5000 для автоматичної зміни слайдів кожні 5 секунд
+  },
+  pagination: {
+    el: '.baner .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.baner .swiper-pagination',
+    prevEl: '.baner .swiper-pagination',
+  },
 });
+console.log('love');
+
 
